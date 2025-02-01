829d081e78b38fe08681cf2cd9b4f60becded845
5-abdulsami <5abdulsami2004@gmail.com>
2025-02-02 03:00:41 +0500

    changed app name and description

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:picpress/main_screen.dart';
+import 'package:piximize/main_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'PicPress',
+        title: 'Piximize',
         theme: ThemeData(
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,

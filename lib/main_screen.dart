04b8481603affc4843862b9ae6212180842cc562
5-abdulsami <5abdulsami2004@gmail.com>
2025-02-04 22:29:40 +0500

    pick image

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -1,8 +1,12 @@
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:piximize/services/drag_drop_widget.dart';
 import 'package:piximize/utils/colors.dart';
 import 'package:piximize/widgets/app_bar.dart';
 import 'package:piximize/widgets/piximize.dart';
+import 'package:image_picker_web/image_picker_web.dart';
 
 class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
@@ -12,6 +16,7 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
+  Uint8List? imageBytes;
   @override
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
@@ -33,7 +38,7 @@ class _MainScreenState extends State<MainScreen> {
             ),
             Container(
               width: size.width * 0.5,
-              height: size.height * 0.5,
+              height: size.height * 0.6,
               decoration: BoxDecoration(
                 color: primaryColor.withValues(alpha: 0.3),
                 borderRadius: BorderRadius.circular(20),
@@ -43,6 +48,22 @@ class _MainScreenState extends State<MainScreen> {
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
+                    imageBytes != null
+                        ? Image.memory(
+                            imageBytes!,
+                            width: 250,
+                            height: 250,
+                            filterQuality: FilterQuality.high,
+                            fit: BoxFit.contain,
+                          )
+                        : DragDropWidget(
+                            onFileDropped: (bytes) {
+                              setState(() => imageBytes = bytes);
+                            },
+                          ),
+                    SizedBox(
+                      height: size.height * 0.02,
+                    ),
                     ElevatedButton.icon(
                       onHover: (value) => setState(() {}),
                       style: ElevatedButton.styleFrom(
@@ -52,7 +73,16 @@ class _MainScreenState extends State<MainScreen> {
                           borderRadius: BorderRadius.circular(10),
                         ),
                       ),
-                      onPressed: () {},
+                      onPressed: () async {
+                        Uint8List? bytesFromPicker =
+                            await ImagePickerWeb.getImageAsBytes();
+
+                        if (bytesFromPicker != null) {
+                          setState(() {
+                            imageBytes = bytesFromPicker;
+                          });
+                        }
+                      },
                       icon: Icon(
                         Icons.image,
                         color: whiteColor,
--- lib/widgets/app_bar.dart
+++ lib/widgets/app_bar.dart
@@ -16,7 +16,7 @@ AppBar customAppBar(BuildContext context) {
       ),
     ),
     toolbarHeight: size.height * 0.1,
-    backgroundColor: whiteColor,
+    backgroundColor: const Color.fromARGB(255, 56, 55, 68),
     title: Row(
       children: [
         SizedBox(width: size.width * 0.01),
@@ -29,7 +29,7 @@ AppBar customAppBar(BuildContext context) {
         Text(
           'Piximize',
           style: GoogleFonts.pacifico(
-              color: primaryColor, fontSize: size.width * 0.018),
+              color: whiteColor, fontSize: size.width * 0.018),
         ),
       ],
     ),
